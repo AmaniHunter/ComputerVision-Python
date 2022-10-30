644d5faa2692d79b72eac1924aab72775992eae1
Amani Hunter <amanikhunter@gmail.com>
2022-10-30 15:20:20 -0400

    Removed line

--- Assignment1/CaptureImage.py
+++ Assignment1/CaptureImage.py
@@ -13,7 +13,6 @@ streams.append('isp')
 | byte0[7:0] | byte1[7:0] | byte2[7:0] | byte3[7:0] |          byte4[7:0]             |
 |    p0[9:2] |    p1[9:2] |    p2[9:2] |    p3[9:2] | p3[1:0],p2[1:0],p1[1:0],p0[1:0] |
 '''
-# Optimized with 'numba' as otherwise would be extremely slow (55 seconds per frame!)
 @nb.njit(nb.uint16[::1] (nb.uint8[::1], nb.uint16[::1], nb.boolean), parallel=True, cache=True)
 def unpack_raw10(input, out, expand16bit):
     lShift = 6 if expand16bit else 0
