b423b61db6e9552daa77a1f5bdb26cd9c269e5a6
Amani Hunter <amanikhunter@gmail.com>
2022-10-31 19:24:13 -0400

    Added file for Canny Edge Detection.

--- Assignment2/#1/CannyEdgeDetection.py
+++ Assignment2/#1/CannyEdgeDetection.py
@@ -1,9 +1,10 @@
-import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-captured_image = cv2.imread("")
-edge_image = cv2.Canny(captured_image,180,200)
-plt.imshow(captured_image)
-plt.imshow(edge_image,cmap="gray")
+captured_image = cv2.imread("Amani_capture_isp_1.png",0)
+edges = cv2.Canny(captured_image, 180, 200)
+plt.subplot(121),plt.imshow(captured_image,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 plt.show()
